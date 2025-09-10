d230aaabb702a7bbda3fdab117dea5fca1a0724d
Yaroslav <iaroslav.bierioza@gmail.com>
2025-09-10 20:10:08 +0300

    some tasks by js

---
+++ about.js
@@ -0,0 +1,26 @@
+const quotes = Array.from(document.querySelectorAll('.quote'));
+let count = 0;
+quotes.forEach((quote) => {
+    count += 1;
+    quote.innerText = count + '. ' + quote.innerText;
+})
+
+const tasks = Array.from(document.querySelectorAll('.task'));
+let count_task = 1;
+tasks.forEach((task) => {
+    task.innerText = count_task + '. ' + task.innerText;
+    count += 1;
+})
+
+const text = Array.from(document.querySelectorAll(".lorem"));
+const colors = ["yellow", "green", "blue"];
+for (let i = 0; i < text.length; i++) {
+    text[i].style.backgroundColor = colors[i % 3];
+}
+
+const links = Array.from(document.querySelectorAll(".link a"))
+links.forEach((link) => {
+    if (link.href.startsWith("https")) {
+        link.innerText = link.innerText + " (external)"
+    }
+})
